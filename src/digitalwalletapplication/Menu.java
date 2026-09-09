db505837fde985a02a26d643bbb4d29bcf9f0dcf
Muhammad Ammar <muhdammar.azizan@gmail.com>
2026-09-10 00:56:15 +0800

    Add MySQL Connector/J driver to fix database connection
       - Root cause: MySQL Connector/J JDBC driver was never added to project
         classpath (javac.classpath was empty), causing MyConnection.getConnection()
         to silently return null and crash the app with NullPointerException
       - Added mysql-connector-j-26.7.0.jar to lib/ folder, bundled with project
       - Updated nbproject/project.properties javac.classpath to include the JAR
       - Updated .gitignore to allow lib/*.jar (previously blocked by global *.jar rule)
       - Verified: Register.java now successfully connects and inserts into database

--- src/digitalwalletapplication/Menu.java
+++ src/digitalwalletapplication/Menu.java
@@ -165,7 +165,7 @@ public class Menu extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        TrannsactionGUI tr = new TrannsactionGUI();
+        TransactionGUI tr = new TransactionGUI();
         tr.setVisible(true);
         tr.pack();
         tr.setLocationRelativeTo(null);
--- src/digitalwalletapplication/TransactionGUI.java
+++ src/digitalwalletapplication/TransactionGUI.java
@@ -18,12 +18,12 @@ import javax.swing.JOptionPane;
  *
  * @author ASUS
  */
-public class TrannsactionGUI extends javax.swing.JFrame {
+public class TransactionGUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form TrannsactionGUI
+     * Creates new form TransactionGUI
      */
-    public TrannsactionGUI() {
+    public TransactionGUI() {
         initComponents();
     }
 
@@ -241,20 +241,20 @@ public class TrannsactionGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TrannsactionGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TransactionGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TrannsactionGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TransactionGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TrannsactionGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TransactionGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TrannsactionGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TransactionGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TrannsactionGUI().setVisible(true);
+                new TransactionGUI().setVisible(true);
             }
         });
     }
